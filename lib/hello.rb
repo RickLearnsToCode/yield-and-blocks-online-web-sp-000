00a4d4c0638b1c5c3465ecb8bea254a097ff7d49
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-24 03:57:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,13 @@
 def hello_t(passed_in)
+  block_given? ? passed_in.each {|i| yield i} : puts "Hey! No block was given!"
+
+=beginning
   if block_given?
     passed_in.each {|i| yield i}
   else
     puts "Hey! No block was given!"
   end
+=end
 end
 
 # call your method here!
