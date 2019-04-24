2c4dd1f70ef5c987749e23638bef2980be224171
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-24 03:59:54 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,5 @@
 def hello_t(passed_in)
-  block_given? ? passed_in.each {|i| yield i} : puts "Hey! No block was given!"
+  block_given? passed_in.each {|i| yield i} : puts "Hey! No block was given!"
 
 =beginning
   if block_given?
