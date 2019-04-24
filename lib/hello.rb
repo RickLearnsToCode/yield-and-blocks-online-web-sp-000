e5c4f53609a9c43957b93f493dca5fae23c2e199
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-24 03:47:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,5 @@
 def hello_t(passed_in)
-  block_given? ? passed_in.each {|i| yield(i)} : return nil
+  block_given? ? passed_in.each {|i| yield(i)} : puts "try again"
 end
 
 # call your method here!
